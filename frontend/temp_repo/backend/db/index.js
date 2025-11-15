52d3de677120b0544062c09cb10eaf1221afa010
Harihar Bajpai <143341040+hariharbajpai@users.noreply.github.com>
2025-11-15 19:09:05 +0530

    Delete frontend directory

--- frontend/temp_repo/backend/db/index.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export default async function connectDB() {
-  const uri = process.env.MONGODB_URI;
-  if (!uri) throw new Error('MONGODB_URI missing in .env');
-
-  await mongoose.connect(uri, { autoIndex: true });
-  console.log('Mongo connected');
-}
